d3cffedf7b5ae58fa4b5ee0afe626a3db887a9af
4trastos <info@4trastos.com>
2025-09-17 11:18:43 +0200

    work in progress

---
+++ incl/ft_strace.h
@@ -0,0 +1,10 @@
+#ifndef FT_STRACE_H
+# define FT_STRACE_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdbool.h>
+# include <unistd.h>
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "incl/ft_strace.h"
+#include "lib/printf/ft_printf.h"
+
+int main(int argc, char **argv)
+{
+    return (0);
+}
