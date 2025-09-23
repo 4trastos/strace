65ddf48041e8139dcc8d5f5b1f3364dbd9e647f6
4trastos <info@4trastos.com>
2025-09-24 01:29:08 +0200

    moving forward

--- incl/ft_strace.h
+++ incl/ft_strace.h
@@ -43,15 +43,25 @@ struct user_regs_struct_32
 };
 # endif
 
+typedef enum
+{
+    INT,
+    STRING,
+    POINTER,
+    VOID,
+}   e_arg_type;
+
 typedef struct s_syscall_info
 {
-    int     arch;
-    long    syscall_numb;
-    long    return_value;
-    long    arguments[6];
-    char    *path;
-    char    **command_path;
-    char    *binary;
+    int         arch;
+    long        syscall_numb;
+    long        return_value;
+    long        arguments[6];
+    char        *path;
+    char        **command_path;
+    char        *binary;
+    char        syscall_name;
+    e_arg_type  type;
 }   t_syscall_info;
 
 typedef struct s_signals
